7194cb6c47d1f6fb9bf9226dae39c0e9d3c215e1
aluisaoliveira <aluisaoliveira@ipvc.pt>
2022-10-25 10:05:52 +0100

    UP

--- Clean_Dataset.py
+++ Clean_Dataset.py
@@ -1,18 +1,6 @@
 import csv
 
-f = open('Clean_Dataset.csv')
-
-csv_f = csv.reader(f)
-
-data=[]
-
-for row in csv_f:
-    data.append(row)
-
-f.close()
-
-print(data[1:])
-
+#Converter string para XML
 def convert_row(row):
     return  """<Plane>
 <airline>%s</airline>
@@ -28,5 +16,37 @@ def convert_row(row):
 <price>%s</price>
 </Plane>""" % (row[0], row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10])
 
-with open('output.xml', 'w') as f:
-    f.write('\n'.join([convert_row(row) for row in data]))
+def mainXML(fich):
+    print(fich)
+    # Abrir CVS e ler dados
+    f = open(fich)
+    f_csv = csv.reader(f)
+    data = []
+
+
+    # Substituir caracteres invalidos por algo valido
+    for row in f_csv:
+        tags = row
+
+        for i in range(len(tags)):
+            row[i] = row[i].replace('&', "and")
+        data.append(row)
+
+    f.close()
+    # Test only
+    #print('\n'.join([convert_row(row) for row in data[1:]]))
+
+    # Escreve o XML num ficheiro
+    with open('output.xml', 'w') as f: f.write(
+        '<xml>\n' + '\n'.join([convert_row(row) for row in data[1:]]) + '\n</xml>\n')  # abrir ficheiro xml
+# Ao abrir tem que verificar se o "no" principal esta aberto e fechado.
+
+if __name__ == '__main__':
+    try:
+        mainXML("./Clean_Dataset.csv")
+    # Catch exceptions
+    except (Exception, ArithmeticError) as e:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(e).__name__, e.args)
+        print(message)
+
